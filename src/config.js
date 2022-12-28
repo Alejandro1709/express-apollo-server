b3e3198ce826cc60a66e1f6c30e77b315aa2c435
Alejandro Rodriguez <alejandro.lr.developer@hotmail.com>
2022-12-28 13:02:30 -0500

    Add: Mongo URI to connection string

--- src/config.js
+++ src/config.js
@@ -3,3 +3,5 @@ import dotenv from 'dotenv';
 dotenv.config({ path: '../.env' });
 
 export const PORT = process.env.PORT || 4000;
+export const MONGO_URI =
+  process.env.MONGO_URI || 'mongodb://localhost:27017/shopping';
--- src/db/index.js
+++ src/db/index.js
@@ -1,8 +1,9 @@
 import mongoose from 'mongoose';
+import { MONGO_URI } from '../config.js';
 
 const connectDb = async () => {
   try {
-    await mongoose.connect('mongodb://localhost:27017/shopping');
+    await mongoose.connect(MONGO_URI);
     console.log('✅ MongoDB connected');
   } catch (error) {
     console.log('❌ MongoDB connection failed');
