0639c82310c003936cd4ea0914fe9ae8421442de
Alejandro Rodriguez <alejandro.lr.developer@hotmail.com>
2022-12-28 11:43:25 -0500

    Add: extended express server with apollo server

--- src/app.js
+++ src/app.js
@@ -1,11 +1,31 @@
 import express from 'express';
+import { ApolloServer } from 'apollo-server-express';
+import { typeDefs } from './schemas/index.js';
+import { resolvers } from './resolvers/index.js';
 import { PORT } from './config.js';
 
 const app = express();
 
+app.get('/', (_req, res) => {
+  res.status(200).json({ message: 'Hello!' });
+});
+
 export default app;
 
 const start = async () => {
+  const apolloServer = new ApolloServer({
+    typeDefs,
+    resolvers,
+  });
+
+  await apolloServer.start();
+
+  apolloServer.applyMiddleware({ app });
+
+  app.all('*', (_req, res) => {
+    res.status(404).json({ message: 'Not Found!' });
+  });
+
   app.listen(PORT, () =>
     console.log(`🚀 Server is up and running on port: ${PORT}`)
   );
